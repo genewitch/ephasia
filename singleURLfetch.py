b19c2f397ed699f803075f41faded23c95b96e4e
genewitch <genewitch@gmail.com>
2013-09-09 02:49:04 -0700

    Update singleURLfetch.py
    Rather than parse URLs i search for double slashes in anchor tags' href arguments. I know i'll hate myself later if i don't comment this so here's this. string.partition will split a string into a tuple based on 1 argument, a separator. in this case, i use double slashes '//' the resulting tuple will have empty [1] and [2] indices. I then verify that one of the first two chars of the URI is a forward slash.
    As of now this rejects self-referencing links ("https://github.com/genewitch/ephasia/edit/master/singleURLfetch.py"), mailto, and another weird glitch.

--- singleURLfetch.py
+++ singleURLfetch.py
@@ -1,18 +1,41 @@
+foosite = 'http://www.gentoo.org'
+
 import pycurl
 c = pycurl.Curl()
-c.setopt(pycurl.URL, "http://www.gentoo.org")
+c.setopt(pycurl.URL, foosite)
 c.setopt(pycurl.HTTPHEADER, ["Accept:"])
+
 import StringIO
 b = StringIO.StringIO()
 c.setopt(pycurl.WRITEFUNCTION, b.write)
 c.setopt(pycurl.FOLLOWLOCATION, 1)
 c.setopt(pycurl.MAXREDIRS, 5)
 c.perform()
+
 from bs4 import BeautifulSoup
+
 import redis
-souper = BeautifulSoup(b.getvalue())
+
+souper = BeautifulSoup(b.getvalue(), 'lxml')
 '''for link in souper.find_all('a'):
     print(link.get('href'))'''
-r = redis.Redis(host='localhost', port=6379, db=1)
+
+r = redis.Redis(host='localhost', port=6379, db=2)
+
 for link in souper.find_all('a'):
-        r.lpush('www.gentoo.org', link.get('href'))
+
+        resolve = link.get('href')
+        try:
+                if resolve.partition('//')[1] == '':
+                        if resolve.rfind('/') <= 1:
+                                print ("bad http URL found", foosite, resolve)
+                        else:
+                                r.lpush(foosite, foosite + resolve)
+                else:
+                        r.lpush(foosite, resolve)
+        except:
+                print('error in resolve', foosite, resolve)
+                break
+
+print(r.lrange(foosite, 0, -1))
+r.delete(foosite)
